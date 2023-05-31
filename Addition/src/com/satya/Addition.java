968e34c24e4dbb77d34f66a66742a33510d58747
RamaDevi01 <erriramadevi05@gmail.com>
2023-05-31 15:19:59 +0530

    first addition

---
+++ Addition/src/com/satya/Addition.java
@@ -0,0 +1,13 @@
+package com.satya;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1 = 20;
+		int num2 = 40;
+		int result= num1+num2;
+		System.out.println("Addition Of two Numbers" + result);
+
+	}
+
+}
